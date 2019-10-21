f06c1f86cf95de5cfeb172dfd90c67664cf18267
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-21 15:14:10 +1100

    Huft

---
+++ Assets/Tests/SimpleTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SimpleTest
+{
+    [Test]
+    public void SimpleUnityTest()
+    {
+        Assert.IsTrue(false);
+    }
+
+    [UnityTest]
+    public IEnumerator SimpleUnityTestTools()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Assert.IsTrue(false, "Unity Test Failed");
+    }
+}
